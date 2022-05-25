a29fbf966b47efdcc2821ad186a0b72c047db380
knagzai <59835969+OSGZ@users.noreply.github.com>
2022-05-25 08:21:16 +0800

    Update footer.php
    更新了效果

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <a id="to_top" href="#" class="to_top" style="display: block;"><i class="ri-arrow-up-line"></i></a> 
             <div class="footer">
                  <nav class="nav">
-                 © <?php echo date('Y'); ?> | Theme is <a href="https://www.osgz.com/" target="_blank">Ying</a></br>
+                 © <?php echo date('Y'); ?> | Theme is <a href="https://github.com/OSGZ/Ying/" target="_blank">Ying</a></br>
                  </nav>
             </div>
         </div>
