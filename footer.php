c5b7ee6de369c75b0e3f90c0c837fbd510bd897d
Sio <59835969+Siooooooooo@users.noreply.github.com>
2024-11-22 20:58:02 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
 <a id="to_top" href="#" class="to_top" style="display: block;"><i class="ri-arrow-up-line"></i></a> 
             <div class="footer">
                  <nav class="nav">
-                 © 2016 - <?php echo date('Y'); ?> Theme is <a href="https://github.com/Siuyo/Ying" target="_blank">颖</a></br>
+                 © 2016 - <?php echo date('Y'); ?> Theme is <a href="https://github.com/Siooooooooo/Ying/" target="_blank">颖</a></br>
                 <a href="https://beian.miit.gov.cn/" target="_blank"><span><?php $this->options->footerbeian(); ?></span></a></br>
                 <svg class="icon" aria-hidden="true">
                      <use xlink:href="#icon-cn"></use>
